971c5ec9d87b1be8bc42cc526b366fe0b769a142
vasiatca <vasiatca@yandex.ru>
2022-07-19 21:48:10 +0300

    landing (create Techs)

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,7 +4,7 @@ import Header from "../Header/Header";
 import Promo from "../Promo/Promo";
 // import NavTab from "../NavTab/NavTab";
 import AboutProject from "../AboutProject/AboutProject";
-// import Techs from "../Techs/Techs";
+import Techs from "../Techs/Techs";
 // import AboutMe from "../AboutMe/AboutMe";
 // import Portfolio from "../Portfolio/Portfolio";
 
@@ -15,7 +15,7 @@ function Main() {
       <Promo />
       {/* <NavTab /> */}
       <AboutProject />
-      {/* <Techs /> */}
+      <Techs />
       {/* <AboutMe /> */}
       {/* <Portfolio /> */}
     </>
